f7cb355dcc5ce897e5de81c5a4469acabc4a4685
bharathchandu302 <bharathchandu302@gmail.com>
2025-09-26 18:41:32 +0530

    aading logs

--- 4-logs.sh
+++ 4-logs.sh
@@ -6,52 +6,48 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Run the script with root Privilages"
+    exit 1
+fi
+
 LOG_FOLDER="/var/log/shell-script"
 SCRIPT_NAME="$( echo $0 | cut -d "." -f1 )"
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOG_FOLDER
-echo "Script execution time is: $(date)" | tee -a $LOG_FILE
-
-if [ $USERID -ne 0 ]; then
-    echo "ERROR:: Please run the script with root privilages"
+VALIDATE() {
+if [ $1 -ne 0 ]; then
+    echo "ERROR: install $2 is failure"
     exit 1
+else
+    echo "installing  $2 is success"
 fi
-
-VALIDATE(){
-    if [ $1 -ne 0 ]; then
-        echo "ERROR:: installing $2 is failure"
-        exit 1
-    else
-        echo "instaling $2 is success"
-    fi
 }
 
-dnf list installed mysql &>>$LOG_FILE
-# install it is not found
+# if package is not found
+dnf list installed Mysql &>>$LOG_FILE
 if [ $? -ne 0 ]; then
     dnf install mysql -y &>>$LOG_FILE
-    VALIDATE $? "mysql"
+    VALIDATE $? "MYSQL"
 else
-    echo -e "mysql already installed.. $Y SKIPPING $N"
+    echo -e "mysql already installed..$Y SKIPPING $N"
 fi
 
-dnf list installed nginx &>>$LOG_FILE
-# install is not found
+#if package is not found
+dnf list installed Nginx &>>$LOG_FILE
 if [ $? -ne 0 ]; then
     dnf install nginx -y &>>$LOG_FILE
     VALIDATE $? "NGINX"
 else
-    echo -e "nginx already installed.. $Y SKIPPING $N"
+    echo -e "nginx already installed... $Y SKIPPING $N"
 fi
 
-dnf list installed  python3 &>>$LOG_FILE
-# install is not found
+#if package is not found
+dnf list installed Python3 &>>$LOG_FILE
 if [ $? -ne 0 ]; then
     dnf install python3 -y &>>$LOG_FILE
     VALIDATE $? "PYTHON3"
 else
-    echo -e "python3 already installed.. $Y SKIPPING $n"
+    echo -e "python3 alreday installed ..$Y SKIPPING $N"
 fi
-
-
