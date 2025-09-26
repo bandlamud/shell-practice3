572e934e77cad05e2cb4e74ccc0734ef179e0d22
bharathchandu302 <bharathchandu302@gmail.com>
2025-09-26 16:28:37 +0530

    identifying root access

--- 1-user-installpkg.sh
+++ 1-user-installpkg.sh
@@ -4,12 +4,14 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: Run the script with root Privilages"
+    exit 1
 fi
 
 # Install the packae
 dnf install mysql -y
 if [ $? -ne 0 ]; then
     echo "ERROR: install mysql is failure"
+    exit 1
 else
     echo "install my sql is success"
 fi
