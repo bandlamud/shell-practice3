f2e8e7f45688c3b27fa4d479761e7f599cb8d96e
bharathchandu302 <bharathchandu302@gmail.com>
2025-09-26 16:34:53 +0530

    installing nginx and python3 package installation

--- 1-user-installpkg.sh
+++ 1-user-installpkg.sh
@@ -15,3 +15,17 @@ if [ $? -ne 0 ]; then
 else
     echo "install my sql is success"
 fi
+
+dnf install nginx -y
+if [ $? -ne 0 ]; then
+    echo "installing nginx is failure"
+else
+    echo "installing nginx is success"
+fi
+
+dnf install python3
+if [ $? -ne 0 ]; then
+    echo "installing python3 is failure"
+else
+    echo "installing python3 is success"
+fi
