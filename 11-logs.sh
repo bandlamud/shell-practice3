335a0242ab7150d8443f0006a4f9f83799d278cc
bharathchandu302 <bharathchandu302@gmail.com>
2025-09-27 20:36:50 +0530

    logs

--- 11-logs.sh
+++ 11-logs.sh
@@ -9,7 +9,7 @@ USERID=$(id -u)
 
 LOG_FOLDER="/var/log/shell-script"
 SCRIPT_NAME= "$( echo $0 | cut -d "." -f1 )"
-LOG_FILE=($LOG_FOLDER/$SCRIPT_NAME.log)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
 mkdir -p $LOG_FOLDER
 
 if [ $USERID -ne 0 ]; then
