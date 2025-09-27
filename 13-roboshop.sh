9180678206937ae75ef592ff1d347816459ef2e7
bharathchandu302 <bharathchandu302@gmail.com>
2025-09-27 23:37:51 +0530

    using shell to create ec2 instance

--- 13-roboshop.sh
+++ 13-roboshop.sh
@@ -2,6 +2,8 @@
 
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0f110578dbed67965"
+ZONE_ID="Z00363011Z6DCTPYPRFG8" # replace with your ID
+DOMAIN_NAME="daws89s.fun"
 
 for instance in $@
 do
@@ -10,10 +12,30 @@ do
     # get Private ip
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws89s.fun
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        RECORD_NAME="$DOMAIN_NAME" # daws89s.fun
     fi
         echo "$instance: $IP"
+       aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record set"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }
+    '   
 
 done
 
