f12fedf6d5f3d5509bbaf1650522897536834bd5
bharathchandu302 <bharathchandu302@gmail.com>
2025-09-27 20:09:31 +0530

    checking root privilages to run the script

--- 7-user.sh
+++ 7-user.sh
@@ -4,12 +4,14 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: Please run the script with root Privilages"
+    exit 1
 fi
 
 # installed packages
 dnf install mysql -y
 if [ $? -ne 0 ]; then
     echo "installing mysql is failure"
+    exit 1
 else
     echo "installing mysql is success"
 fi
