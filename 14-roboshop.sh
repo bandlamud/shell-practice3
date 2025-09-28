6c6c2f6cfc9b979c54e42b2e0201c994e2ba5d14
bharathchandu302 <bharathchandu302@gmail.com>
2025-09-29 02:04:26 +0530

    created shell script for ec2 instances

--- 14-roboshop.sh
+++ 14-roboshop.sh
@@ -10,6 +10,7 @@ if [ $instance != "frontend"]; then
     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
 else
     IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+fi
     echo "$instance: $IP"
 done
 
